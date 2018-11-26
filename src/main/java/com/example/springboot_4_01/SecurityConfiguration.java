646870aa7f998121012e5e177cfc104e611cf766
tamiawhite <tamia327@gmail.com>
2018-11-26 16:09:59 -0500

    version 4.03

--- src/main/java/com/example/springboot_4_01/SecurityConfiguration.java
+++ src/main/java/com/example/springboot_4_01/SecurityConfiguration.java
@@ -6,13 +6,14 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends
-        WebSecurityConfigurerAdapter{
+        WebSecurityConfigurerAdapter {
 
     @Bean
-    public static BCryptPasswordEncoder passwordEncoder(){
+    public static BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
@@ -20,16 +21,32 @@ public class SecurityConfiguration extends
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers("/")
+                .access("hasAnyAuthority('USER','ADMIN')")
+                .antMatchers("/admin").access("hasAnyAuthority('ADMIN')")
                 .anyRequest().authenticated()
                 .and()
-                .formLogin();
+                .formLogin().loginPage("/login").permitAll()
+                .and()
+                .formLogin().loginPage("/login").permitAll()
+                .and()
+                .logout()
+                .logoutRequestMatcher(
+                        new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login").permitAll();
+
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth)
-        throws Exception{
-        auth.inMemoryAuthentication().withUser("user")
-                .password(passwordEncoder().encode("password"))
-                .authorities("USER");
+            throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("dave").password(passwordEncoder().encode("password"))
+                .authorities("ADMIN")
+                .and()
+                .withUser("user").password(passwordEncoder().encode("password"))
+                .authorities("USER").and().passwordEncoder(passwordEncoder());
+
     }
+
 }
