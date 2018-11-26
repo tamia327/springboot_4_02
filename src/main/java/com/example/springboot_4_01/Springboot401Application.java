6e361a8ed65a4752089f32bc155c08e7722dbc85
Tamia White <tamiawhite@Tamias-MacBook-Pro.local>
2018-11-26 14:53:20 -0500

    Initial commit

---
+++ src/main/java/com/example/springboot_4_01/Springboot401Application.java
@@ -0,0 +1,12 @@
+package com.example.springboot_4_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot401Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot401Application.class, args);
+    }
+}
